288421af12947dcd227c3cc47121c04962fa22a8
Raj <rajdhage2104@gmail.com>
2023-11-02 19:15:15 +0530

    1commit

--- security.tf
+++ security.tf
@@ -25,8 +25,8 @@ resource "aws_security_group" "my-sg1" {
         cidr_block = ["0.0.0.0/0"]
     }
     egress = {
-        from_port = 0
-        to_port = 0
+        from_port = 80
+        to_port = 80
         protocol = "-1"
         cidr_block = ["0.0.0.0/0"]
 
