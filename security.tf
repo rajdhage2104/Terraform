608bcc5f628f2c5dec2d0a1690a8c0a0f3fa9d4d
Raj <rajdhage2104@gmail.com>
2023-11-02 18:03:33 +0530

    2nd commit

--- security.tf
+++ security.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "terra1" {
     ami = "ami-0763cf792771fe1bd"
     instance_type = "t2.micro"
     key_name = "raja"
-    vpc_security_group_ids = "sg-0fd7dbc86fdf7161b"
+    vpc_security_group_ids = "vpc-0bb01c3101a1e600d"
     tags = {
       name = "raj"
       env = "dev"
